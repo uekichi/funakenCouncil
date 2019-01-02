cd774d9d896d9a8e2ec6ebba3edf444bd546a775
uekichi <uekichi@qc.commufa.jp>
2019-01-02 02:43:40 +0000

    タイトルの詳細画面を作成

--- routes/titles.js
+++ routes/titles.js
@@ -6,9 +6,39 @@ const authenticationEnsurer = require('./authentication-ensurer');
 const uuid = require('uuid');
 const Title = require('../models/title');
 const Strategy = require('../models/strategy');
+const User = require('../models/user');
 
 router.get('/new', authenticationEnsurer, (req, res, next) => {
   res.render('new', {user: req.user});
+});
+
+router.get('/:titleId', authenticationEnsurer, (req, res, next) => {
+  Title.findOne({
+    include: [
+      {
+        model: User,
+        attributes: ['userId', 'username']
+      }],
+    where: {
+      titleId: req.params.titleId
+    },
+    order: [['"createdAt"', 'DESC']]
+  }).then((title) => {
+    if (title) {
+      Strategy.findAll({
+        where: {titleId: title.titleId},
+        order: [['"strategyId"', 'ASC']]
+      }).then((strategies) => {
+        res.render('title', {
+          user: res.user, title: title, strategies: strategies, users: [req.user]
+        });
+      });
+    } else {
+      const err = new Error('指定したタイトルは見つかりませんでした');
+      err.status = 404;
+      next(err);
+    }
+  });
 });
 
 // '/'は URL  titles のこと
