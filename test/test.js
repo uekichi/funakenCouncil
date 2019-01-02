60c158be18b9c221bdcbf85b6498dcd0e1357074
uekichi <uekichi@qc.commufa.jp>
2019-01-02 12:59:10 +0000

    タイトルの作成と表示をテストした

--- test/test.js
+++ test/test.js
@@ -2,6 +2,9 @@
 const request = require('supertest');
 const app = require('../app');
 const passportStub = require('passport-stub');
+let User =require('../models/user');
+let Title = require('../models/title');
+let Strategy = require('../models/strategy');
 
 describe('/login', () => {
 
@@ -37,4 +40,49 @@ describe('/login', () => {
     .expect(302, done);
   });
 
+});
+
+describe('/titles', () => {
+  before(() => {
+    passportStub.install(app);
+    passportStub.login({id: 0, username: 'testuser'});
+  });
+
+  after(() => {
+    passportStub.logout();
+    passportStub.uninstall(app);
+  });
+
+  it('予定が作成でき、表示される', (done) => {
+    User.upsert({userId: 0, username: 'testuser'}).then(() => {
+      request(app)
+      .post('/titles')
+      .send({titleName: 'テスト予定１', memo: 'テストメモ１\r\nテストメモ２', strategies: 'テスト候補１\r\nテスト候補２\r\nテスト候補３'})
+      .expect('Location', /titles/)
+      .expect(302)
+      .end((err, res) => {
+        const createdTitlePath = res.headers.location;
+        request(app)
+        .get(createdTitlePath)
+        .expect(/テスト予定１/)
+        .expect(/テストメモ１/)
+        .expect(/テストメモ２/)
+        .expect(/テスト候補１/)
+        .expect(/テスト候補２/)
+        .expect(/テスト候補３/)
+        .expect(200)
+        .end((err, res) => {
+          const titleId = createdTitlePath.split('/titles/')[1];
+          Strategy.findAll({
+            where: {titleId: titleId}
+          }).then((strategies) => {
+            strategies.forEach((s) => {s.destroy();});
+            Title.findById(titleId).then((t) => {t.destroy();});
+          });
+          if (err) return done(err);
+          done();
+        });
+      });
+    });
+  });
 });
